eede9915ee08bda62f4d2b5252b2cdd720521f57
HerbertV <herbert@crehana.com>
2018-04-15 22:21:33 -0500

    Add a better comment

--- models/TextModel.py
+++ models/TextModel.py
@@ -4,7 +4,8 @@ from .BaseModel import BaseModel
 class TextModel(BaseModel):
   '''
     The grade field should have values between [1, 11], where the lowest value represent the first grade in 
-    the peruviand education system and the highest the last grade.
+    the peruviand education system and the highest the last grade. Also, the system divides in two groups this educational leevels, the
+     elementary school (from 1 to 6) and high school (from 7 to 11) grades.
   '''
   title = CharField()
   date_created = DateTimeField(default=datetime.datetime.now)
